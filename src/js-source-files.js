5efaef61bcf61d6519f008f04bf818831171710c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-08 13:24:01 -0500

    filtering files, leaving only the ones tracked in the git repo

--- src/js-source-files.js
+++ src/js-source-files.js
@@ -3,6 +3,8 @@ var check = require('check-more-types');
 var glob = require('glob');
 var q = require('q');
 var folder = require('./folder');
+var isTracked = require('ggit').isTracked;
+var R = require('ramda');
 
 function findFiles(pattern) {
   pattern = pattern || '**/*.js';
@@ -12,13 +14,23 @@ function findFiles(pattern) {
   // console.log(jsFiles.join('\n'));
 }
 
+function leaveTracked(filenames) {
+  la(check.arrayOfStrings(filenames), 'expected list of filenames', filenames);
+  return q.all(filenames.map(isTracked))
+    .then(R.zipObj(filenames))
+    .then(R.pickBy(R.eq(true)))
+    .then(R.keys);
+}
+
 function sourceFiles(folderName, pattern) {
   if (folderName) {
     return folder.to(folderName)
       .then(findFiles.bind(null, pattern))
+      .then(leaveTracked)
       .tap(folder.comeBack);
   }
-  return findFiles(pattern);
+  return findFiles(pattern)
+    .then(leaveTracked);
 }
 
 module.exports = check.defend(sourceFiles,
